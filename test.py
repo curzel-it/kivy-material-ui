4e76ae71de0a70580af2719e01038ce3cc3f2ffe
Federico Curzel <cuuuurzel@gmail.com>
2015-01-18 15:47:15 +0100

    Added testing class and fonts

--- test.py
+++ test.py
@@ -7,9 +7,6 @@ from kivy.uix.boxlayout import BoxLayout
 
 from flatui.flatui import FlatButton, FloatingAction
 
-Builder.load_file( 'test.kv' )
-
-
 class Test( BoxLayout ) :
     
     def __init__( self, **kargs ) :
@@ -18,9 +15,7 @@ class Test( BoxLayout ) :
 
 class TestApp( App ) :
     
-    def build( self ) :
-        self.t = Test()
-        return self.t
+    def build( self ) : return Test()
 
 if __name__ == '__main__':
     TestApp().run()
