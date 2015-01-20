6e2e1bef3bcce5d5777c8117c74af182a7fa8f57
Federico Curzel <cuuuurzel@gmail.com>
2015-01-21 00:45:11 +0100

    Updated test.py

--- test.py
+++ test.py
@@ -6,13 +6,14 @@ from kivy.properties import *
 from kivy.uix.boxlayout import BoxLayout
 
 from flatui.flatui import *
+from flatui.popups import *
 
 class Test( BoxLayout ) :
     
     def __init__( self, **kargs ) :
         super( Test, self ).__init__( **kargs )
 
-    def show_popup( self ) :
+    def show_popup1( self ) :
         p = FlatPopup( 
             size_hint=(0.8,.8), \
             title='Customizable title', \
@@ -21,6 +22,12 @@ class Test( BoxLayout ) :
             title_size=26
         )
         p.open()
+
+    def show_popup2( self ) :
+        AlertPopup( text='This is a quick alert!', cancel_button_title='Cancel' ).open()
+
+    def show_popup3( self ) :
+        OkButtonPopup( text='This is an other shortcut,\nto be used for quick messages' ).open()
 
 
 
