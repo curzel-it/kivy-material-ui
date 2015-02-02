123581367af3e16c28e2938fe8d0c0f253c9328e
Federico Curzel <cuuuurzel@gmail.com>
2015-02-02 22:12:01 +0100

    Fixed ResizeableLabel

--- flatui/labels.py
+++ flatui/labels.py
@@ -1,6 +1,8 @@
 import sys
 sys.path.append( '..' )
 
+from copy import copy, deepcopy
+
 import kivy.graphics as kg
 from kivy.lang import Builder
 from kivy.properties import *
@@ -36,12 +38,35 @@ class BindedLabel( Label ) :
 
 class ResizeableLabel( BindedLabel ) :
     '''
-    Label you can user-resizeable.
+    User-resizeable label.
     '''    
 
-    #hover_color = ListProperty( [.4,.4,.6,.5] )
     hover_color = ListProperty( [0,0,0,1] )
+    '''
+    A widget is displayed to show the new size of the label.
+    It's filled with this color.
+    '''
+
     root_layout = ObjectProperty( None )
+    '''
+    The 'hover' is drawn on the root layout due to possible size mismatch.
+    You'll need to provide a link to your root layout.
+    '''
+
+    on_new_size = ObjectProperty( None )
+    '''
+    Called by on_size method whenever the size of the label changes.
+    '''
+
+    meta = ObjectProperty( None )
+    '''
+    Passed as argument to on_new_size, use it as you wish...
+    '''
+    
+    min_width = NumericProperty( 50 )
+    '''
+    Label minimum width.
+    '''
 
     _o = ListProperty( [0,0] )
     _d = ListProperty( [0,0] )
@@ -54,33 +79,47 @@ class ResizeableLabel( BindedLabel ) :
         self._unique_group = { 'group':'__resizeable_label_%d' % (id(self)) }
 
     def on_touch_down( self, touch ) :
-        self._o = touch.pos
-        self._pivot = self._get_pivot()
+        self._touched = False
+        if ( ( self.pos[0] < touch.pos[0] < self.pos[0]+self.width ) and
+             ( self.pos[1] < touch.pos[1] < self.pos[1]+self.height ) ) : 
+            self._touched = True
+            self._o = touch.pos
+            self._pivot = self._get_pivot()
+            return True
 
     def on_touch_move( self, touch ) :
-        self._d = touch.pos
+        if self._touched :
+            self._d = touch.pos
+            self._hover_size, self._hover_pos = self._get_hover()
 
-        self._hover_size, self._hover_pos = self._get_hover()
+            if self.root_layout :
+                self._clear_canvas()
 
-        if self.root_layout :
-
-            self._clear_canvas()
-
-            with self.root_layout.canvas :
-                kg.Color( *self.hover_color, **self._unique_group )
-                kg.Rectangle( 
-                    size=self._hover_size, \
-                    pos=self._hover_pos, \
-                    **self._unique_group 
-                )
+                with self.root_layout.canvas :
+                    kg.Color( *self.hover_color, **self._unique_group )
+                    kg.Rectangle( 
+                        size=self._hover_size, \
+                        pos=self._hover_pos, \
+                        **self._unique_group 
+                    )
+            return True
 
     def on_touch_up( self, touch ) :
-        self._clear_canvas()
-        self.size = self._hover_size
+        if self._touched :
+            self._clear_canvas()
+            self._o = []
+            if self._hover_size[0] > self.min_width :
+                self._on_size( self.size, self._hover_size )
+            return True
+
+    def _on_size( self, oldsize, newsize ) :
+        print( 'Size changed' )
+        if self.on_new_size : self.on_new_size( oldsize, newsize, self.meta )
+        self.size = copy( newsize )
 
     def _get_pivot( self ) :
             
-        tx, ty = self._o[0]-self.pos[0], self._o[1]-self.pos[1]
+        tx, ty = abs(self._o[0]-self.pos[0]), abs(self._o[1]-self.pos[1])
         ox, oy = tx/self.size[0], ty/self.size[1]
 
         if ox < 0.33 :
@@ -89,78 +128,25 @@ class ResizeableLabel( BindedLabel ) :
             x = 3
         else :
             x = 6
+        return x +1
 
-        if oy < 0.33 :
+        """
+        if oy > 0.66 :
             return x + 0
-        elif oy < 0.66 :
+        elif oy > 0.33 :
             return x + 1
         else :
             return x + 2
-            
+        """
+    
     def _get_hover( self ) :
 
-        size, pos = self.size, self.pos
-        dx = abs( self._d[0] - self._o[0] )
-        dy = abs( self._d[1] - self._o[1] )
+        dx = self._d[0] - self._o[0]
+        dy = self._d[1] - self._o[1]
 
-        if self._pivot == TOP_RIGHT :
-            size[0] = self.size[0] + dx
-            
-        elif self._pivot == RIGHT :
-            size[0] = self.size[0] + dx
-
-        elif self._pivot == BOTTOM_RIGHT :
-            pos[1] = self.pos[1] - dy
-            size[0] = self.size[0] + dx
-            size[1] = self.size[1] - dy
-
-
-        elif self._pivot == TOP :
-            pos[1] = self.pos[1] + dy
-            size[0] = self.size[0] + dx
-            size[1] = self.size[1] + dy
-            
-        elif self._pivot == CENTER :
-            ...
-
-        elif self._pivot == BOTTOM :
-            pos[1] = self.pos[1] - dy
-            size[0] = self.size[0] + dx
-            size[1] = self.size[1] + dy
-
-
-        elif self._pivot == TOP_LEFT :
-            pos[0] = self.pos[0] - dx
-            pos[1] = self.pos[1] + dy
-            size[0] = self.size[0] + dx
-            size[1] = self.size[1] + dy
-            
-        elif self._pivot == LEFT :
-            pos[0] = self.pos[0] - dx
-            size[0] = self.size[0] + dx
-
-        elif self._pivot == BOTTOM_LEFT :
-            pos[0] = self.pos[0] - dx
-            pos[1] = self.pos[1] - dy
-            size[0] = self.size[0] + dx
-            size[1] = self.size[1] + dy
-
-
-        return size, pos
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        if self._pivot == RIGHT :
+            return [self.size[0]+dx, self.size[1]], self.pos
+        return self.size, self.pos
    
     def _clear_canvas( self ) :
         self.root_layout.canvas.remove_group( self._unique_group['group'] )
