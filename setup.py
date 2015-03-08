12447cf9132f2032ca9edfbf9f8cde9a14524e3d
Federico Curzel <cuuuurzel@gmail.com>
2015-03-08 23:29:31 +0100

    Reorganized code to package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name        ='material_ui',
+    version     ='0.1',
+    description ='Material UI for Kivy.',
+    url         ='https://github.com/Cuuuurzel/kivy-material-ui',
+    author      ='Federico Curzel',
+    author_email='cuuuurzel@gmail.com',
+    packages    =['material_ui'],
+    zip_safe    =False
+)
